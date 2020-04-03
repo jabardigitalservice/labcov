936104dc207d86770ec2f841edd82f28d215fff9
43461293 <galprad99@gmail.com>
2020-04-03 19:12:51 +0700

    revisi middleware salah ketik

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,32 @@ Auth::routes();
 Route::get('logout', 'Auth\LoginController@logout');
 /*
 |--------------------------------------------------------------------------
-| Home Routes
+| Register dan Importnya Routes
 |--------------------------------------------------------------------------
 |
 */
 
 Route::get('/', 'HomeController@index')->middleware('auth');
-Route::resource('registrasi', 'RegisterPasienController')->middleware('superadmin','register','auth');
-Route::post('registrasi/{id}','RegisterPasienController@updatex')->middleware('superadmin','register','auth');
-Route::get('registrasi/delete/{id}','RegisterPasienController@delete')->middleware('superadmin','register','auth');
+Route::resource('registrasi', 'RegisterPasienController')->middleware('register','auth');
+Route::post('registrasi/{id}','RegisterPasienController@updatex')->middleware('register','auth');
+Route::get('registrasi/delete/{id}','RegisterPasienController@delete')->middleware('register','auth');
 
 
-Route::post('import/reg','ImportExportController@importpasien')->middleware('superadmin','register','auth');
-Route::get('import','ImportExportController@indeximport')->middleware('superadmin','register','auth');
+Route::post('import/reg','ImportExportController@importpasien')->middleware('register','auth');
+Route::get('import','ImportExportController@indeximport')->middleware('register','auth');
 
+/*
+|--------------------------------------------------------------------------
+| Pengambilan Sampel dan Importnya Routes
+|--------------------------------------------------------------------------
+|
+*/
 
-Route::resource('pengambilansampel', 'PengambilanSampleController')->middleware('lab1','auth');
+Route::get('pengambilansampel','PengambilanSampleController@index')->middleware('lab1','auth');
+Route::get('pengambilansampel/ambil/{noreg}','PengambilanSampleController@create')->middleware('lab1','auth');
+Route::post('pengambilansampel/ambil','PengambilanSampleController@store')->middleware('lab1','auth');
+
+Route::get('pengambilansampel/edit/{noreg}','PengambilanSampleController@edit')->middleware('lab1','auth');
+Route::post('pengambilansampel/update','PengambilanSampleController@update')->middleware('lab1','auth');
+
+Route::get('pengambilansampel/delete/{id}','PengambilanSampleController@delete')->middleware('lab1','auth');
