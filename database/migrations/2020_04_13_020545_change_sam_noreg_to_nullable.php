29e77b3255a383fad5fe2d34235f40f5b7a68e2a
43461293 <galprad99@gmail.com>
2020-04-13 09:28:14 +0700

    ubah sam_noreg menjadi nullable column

---
+++ database/migrations/2020_04_13_020545_change_sam_noreg_to_nullable.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeSamNoregToNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sampel', function (Blueprint $table) {
+            $table->integer('sam_noreg')->unsigned()->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sampel', function (Blueprint $table) {
+            //
+        });
+    }
+}
