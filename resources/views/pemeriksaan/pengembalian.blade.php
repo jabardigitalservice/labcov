9b17d20a75f439e23fdca6d6c1ba008abece219f
43461293 <galprad99@gmail.com>
2020-04-16 15:13:13 +0700

    revisi  pemid  apabila blm ada pemeriksaan

--- resources/views/pemeriksaan/pengembalian.blade.php
+++ resources/views/pemeriksaan/pengembalian.blade.php
@@ -67,8 +67,9 @@
   <input type="hidden" name="note_item_id" value="{{$eksreturn->eks_id}}" >
   <input type="hidden" name="note_item_type" value="22">
   <input type="hidden" name="note_userid" value="{{Auth::user()->id}}">
-@if(!is_null($pem->pem_id) || isset($pem))
+@if(!is_null($pem))
 <input type="hidden" name="pemid" value="{{$pem->pem_id}}">
+@else
 @endif
 
   <div class="form-group row mt-4">
